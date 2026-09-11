bd6e02e7fb9a21d40c969167b6a51587da37f2da
mindspace-lab <belhimerfatine1@gmail.com>
2026-09-11 12:49:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
 const menuButton = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navbar nav");
 
+if (menuButton && navigation) {
 menuButton.addEventListener("click", () => {
-    navigation.classList.toggle("mobile-menu");
+navigation.classList.toggle("mobile-menu");
+});
+}
 });
